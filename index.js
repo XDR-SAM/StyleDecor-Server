50a4dc6a7397f9be679a06ced45851da4056f225
XDR-SAM <tdxfarhan@gmail.com>
2025-12-10 21:45:07 +0600

    middlewirefix

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const axios = require('axios');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174' , 'https://magical-dusk-71097f.netlify.app'],
+  credentials: true
+}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
