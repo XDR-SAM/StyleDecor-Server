e36ccf4b89cd9d628356b32e273114fdcfb428c2
XDR-SAM <tdxfarhan@gmail.com>
2025-12-12 00:06:29 +0600

    frontend url added

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // ============ Middleware ============
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'https://magical-dusk-71097f.netlify.app'],
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'https://magical-dusk-71097f.netlify.app' ,'https://style-decor-sami.vercel.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
