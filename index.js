8556f97544fe50e1434a3a45602c3162fe431975
XDR-SAM <tdxfarhan@gmail.com>
2025-12-12 18:04:04 +0600

    auth stripe update redirection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // ============ Middleware ============
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174', 'https://magical-dusk-71097f.netlify.app' ,'https://style-decor-sami.vercel.app'],
+  origin: [process.env.FRONTEND_URL,'https://style-decor-sami.vercel.app'],
   credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
@@ -914,7 +914,7 @@ app.post('/api/payments/create-checkout-session', verifyToken, async (req, res) 
     const amountInUSD = (amountInBDT / 110).toFixed(2);
     const amountInCents = Math.round(parseFloat(amountInUSD) * 100);
 
-    const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+    const frontendUrl = process.env.FRONTEND_URL || 'https://style-decor-sami.vercel.app';
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
